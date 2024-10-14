4649b0aee7590f8bda990cf8a17a8350a4b7cb72
Andressa Dias <admarin73@gmail.com>
2024-10-13 21:08:21 -0300

    ajustando arquivo de src

---
+++ src/srcAndressa/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace reciclamais_backend.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Noticia> Noticias { get; set; } 
+        
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
