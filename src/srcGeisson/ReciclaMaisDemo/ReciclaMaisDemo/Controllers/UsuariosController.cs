6515b8f4d978bdca6f269d6e04216583ae58638d
Geisson Oliveira <geissonlucaso@gmail.com>
2024-10-29 23:55:02 -0300

    [srcGeisson] Construção da tela de cadastro de usuário.

--- src/srcGeisson/ReciclaMaisDemo/ReciclaMaisDemo/Controllers/UsuariosController.cs
+++ src/srcGeisson/ReciclaMaisDemo/ReciclaMaisDemo/Controllers/UsuariosController.cs
@@ -18,5 +18,24 @@ namespace ReciclaMaisDemo.Controllers
             var dados = await _context.Usuarios.ToListAsync();
             return View(dados);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Usuarios.Add(usuario);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
     }
 }
