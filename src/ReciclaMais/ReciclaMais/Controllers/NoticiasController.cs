73c95ccff0a38583017c06c1590a4f6ae4ea2537
Andressa Dias <admarin73@gmail.com>
2024-11-14 11:55:49 -0300

    Criado a inserção da imagem no "create"

--- src/ReciclaMais/ReciclaMais/Controllers/NoticiasController.cs
+++ src/ReciclaMais/ReciclaMais/Controllers/NoticiasController.cs
@@ -1,17 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReciclaMais.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ReciclaMais.Controllers
 {
     public class NoticiasController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public NoticiasController(AppDbContext context)
+        public NoticiasController(AppDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
+
         public async Task<IActionResult> Index()
         {
             var dados = await _context.Noticias.ToListAsync();
@@ -25,17 +32,38 @@ namespace ReciclaMais.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(Noticia noticia)
+        public async Task<IActionResult> Create(Noticia noticia, IFormFile Imagem)
         {
             if (ModelState.IsValid)
             {
+                if (Imagem != null && Imagem.Length > 0)
+                {
+                    // Defina o caminho da pasta para salvar a imagem
+                    string uploadFolder = Path.Combine(_webHostEnvironment.WebRootPath, "images");
+                    string uniqueFileName = $"{Guid.NewGuid()}_{Imagem.FileName}";
+                    string filePath = Path.Combine(uploadFolder, uniqueFileName);
+
+                    // Salve a imagem na pasta definida
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await Imagem.CopyToAsync(fileStream);
+                    }
+
+                    // Armazene o caminho relativo da imagem para ser salvo no banco de dados
+                    noticia.Imagem = $"/images/{uniqueFileName}";
+                }
+
+                // Salva os dados da notícia no banco de dados
                 _context.Noticias.Add(noticia);
                 await _context.SaveChangesAsync();
+
                 return RedirectToAction("Index");
             }
 
             return View(noticia);
         }
+
+
 
         public async Task<IActionResult> Edit(int? id)
         {
